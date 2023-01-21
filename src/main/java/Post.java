66114086cb5118eef9864b4889fc3277e7b4c591
Anastasia test <gorandi@inbox.ru>
2023-01-21 20:24:32 +0300

    add Api

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name;
+    public String patronymic;
+    public String surname;
+    public String phone;
+    public String pasport;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
